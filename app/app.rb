e06ce8e0eb6eac1d6bfc0554fb4e4fce56e39e26
Guillermo <mrog0@outlook.com>
2017-06-04 23:23:27 +0100

    Sign up feature - Add controller route to sign up and confirm signing up with the   database records - Add views for signing up and confirmation - Modification in feature testing for signing up - Add User model

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,22 @@
 require 'sinatra/base'
+require_relative './data_mapper_setup.rb'
 
 class Chitter < Sinatra::Base
+  get '/' do
+  end
+
   get '/sign_up' do
-    erb :sign_up
+    erb :'sign_up/sign_up'
+  end
+
+  post '/sign_up' do
+    user = User.new(email: params[:email], username: params[:username], name: params[:name], password: params[:password])
+    user.save
+    redirect('/confirmation')
+  end
+
+  get '/confirmation' do
+    @user = User.last
+    erb :'sign_up/confirmation'
   end
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,10 +1,14 @@
 feature 'Sign up to Chitter' do
   scenario 'signing up' do
-    visit ('/sign_up')
+    visit '/sign_up'
     expect(page.status_code).to eq 200
-    expect(page).to have_field('email')
-    expect(page).to have_field('username')
-    expect(page).to have_field('name')
-    expect(page).to have_field('password')
+    fill_in 'email', with: 'youremail@address.com'
+    fill_in 'username', with: 'yourusername'
+    fill_in 'name', with: 'yourname'
+    fill_in 'password', with: 'apassword'
+    click_button 'Submit'
+    expect(page).to have_content('youremail@address.com')
+    expect(page).to have_content('yourusername')
   end
+
 end
